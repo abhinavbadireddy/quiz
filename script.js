5002317286171b1377c10bd32142acf949219cdd
Abhinav Badireddy <87709968+abhinavbadireddy@users.noreply.github.com>
2022-09-11 18:43:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,130 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    document.getElementById('score').innerHTML = "Your score: " + score.length
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function result(correct) {
+    var score = new Array()
+    if (correct) {
+        score.push("1")
+    } else {
+    }
+    // function getScore(score, value) {
+    //     var count = 0
+    //     score.forEach((v) => (v === value && count++));
+    //     return count;
+    // }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What is the full form of HTML?',
+    answers: [
+      { text: 'Hyper Text Processor', correct: false },
+      { text: 'Hyper Text Markup Language', correct: true },
+      { text: 'Hyper Tool Multi Language', correct: false },
+      { text: 'Hyper Text Multiple Language', correct: false }
+    ]
+  },
+  {
+    question: 'What does XML stand for?',
+    answers: [
+      { text: 'eXamine Multiple Language', correct: false },
+      { text: 'eXecutable Multiple Language', correct: false },
+      { text: 'eXtensible Markup Language', correct: true },
+      { text: 'eXTra Multi-Program Language', correct: false }
+    ]
+  },
+  {
+    question: 'Is web development fun?',
+    answers: [
+      { text: 'Kinda', correct: false },
+      { text: 'YES!!!', correct: true },
+      { text: 'Um no', correct: false },
+      { text: 'IDK', correct: false }
+    ]
+  },
+  {
+    question: 'What is 4 * 2?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '8', correct: true },
+      { text: '24', correct: false },
+      { text: '42', correct: false }
+    ]
+  }
+]
